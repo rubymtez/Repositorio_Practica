14e54178625097851268537ed7331576c766e14d
Rubén <Rubn@192.168.1.4>
2017-06-03 19:23:58 +0200

    Arreglo final

--- src/Hola_Mundo.java
+++ src/Hola_Mundo.java
@@ -5,19 +5,17 @@ public class Hola_Mundo {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
 	}
-<<<<<<< HEAD
 	public void prueba2()
 	{
 		int variable2= 2;
 		variable2 += 2;
-=======
+	}
 		
 		public void prueba()
 		{
 			int variable1 = 1;
 			variable1 += 1;
 		}
->>>>>>> branch 'master' of https://github.com/rubymtez/Repositorio_Practica.git
 	}
 
 
